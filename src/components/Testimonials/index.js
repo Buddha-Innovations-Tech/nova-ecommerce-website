3640d6447ee69de47d245ddc5489cd4c2c74ff8d
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-11-29 11:18:46 +0545

    updated

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -4,33 +4,63 @@ import {  BsStarFill} from "react-icons/bs";
 import TestimonialsRating from './rating';
 
 function Index() {
+
+
+  const Testimonials =[
+    {
+      description : <>  "This product made my dog's coat shinier and boosted his energy like never before"</>,
+    image:Avatar,
+    rate : 5,
+    name:"Cameron Williamson",
+    post:"President of Sales"
+    },
+    {
+      description : <>  "This product made my dog's coat shinier and boosted his energy like never before"</>,
+    image:Avatar,
+    rate : 4,
+    name:"Cameron Williamson",
+    post:"President of Sales"
+    },
+    {
+      description : <>  "This product made my dog's coat shinier and boosted his energy like never before"</>,
+    image:Avatar,
+    rate : 2,
+    name:"Cameron Williamson",
+    post:"President of Sales"
+    }
+  ]
+
+
   return (
     <section className='testimonialWrapper'>
       <Container>
         <section className='layoutTitle mt-3'>
-          <h1>Client Testimonials</h1>
-          <p>Our clients love our work </p>
+          <h1>Happy Clients</h1>
+          <p>See how we help the customers  </p>
         </section>
         <Row>
-          <Col md={4}>
-            <div className='testimonialBox'>
-              <div className='testiDesc'>
-                <div className='testiDesc-quote'>
-                  <TestimonialsRating Rate={4}/>
-                  {/* <div className='testiDesc-quote-stars'>⭐⭐⭐⭐</div> */}
-
-                  <p className='testiDesc-quote-text'>
-                  "This product made my dog's coat shinier and boosted his energy like never before"
-                  </p>
-                </div>
-                <div className='testiImage'>
-                <img className='rectangle' alt='Rectangle' src={Avatar} />
-              </div>
-                <div className='testiDesc-person'>Cameron Williamson</div>
-                <div className='testiDesc-designation'>President of Sales</div>
-              </div>
-            </div>
-          </Col>
+          {Testimonials.map((item,index) => (
+               <Col md={4} key={index}>
+               <div className='testimonialBox'>
+                 <div className='testiDesc'>
+                   <div className='testiDesc-quote'>
+                     <TestimonialsRating Rate={item.rate}/>
+                     {/* <div className='testiDesc-quote-stars'>⭐⭐⭐⭐</div> */}
+   
+                     <p className='testiDesc-quote-text'>
+                     {item.description}
+                     </p>
+                   </div>
+                   <div className='testiImage'>
+                   <img className='rectangle' alt='Rectangle' src={item.image} />
+                 </div>
+                   <div className='testiDesc-person'>{item.name}</div>
+                   <div className='testiDesc-designation'>{item.post}</div>
+                 </div>
+               </div>
+             </Col>
+          ))}
+       
         </Row>
       </Container>
     </section>
