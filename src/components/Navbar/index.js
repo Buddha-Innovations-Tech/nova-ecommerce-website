2d43c7fc55fe27f9d9d10aa2938f4fa36b8562b7
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-12-03 14:44:09 +0545

    font chnaged to open sans , fevicon and title of page chnaged ..subcategory page is not working

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,227 +1,198 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Form } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
-
-import { useDispatch, useSelector } from 'react-redux/es/exports';
-
+import { useDispatch, useSelector } from 'react-redux';
 import BrandImage from '../../assets/images/logo.png';
-import Account from '../../assets/icons/profile.png';
 import Cart from '../../assets/icons/shopping-cart.png';
 import Chevrone from '../../assets/icons/chevron.svg';
 import { getCategoriesAsync } from '../../redux/categorySlice';
 import NavSkeletons from '../Skeletons/NavSkeletons';
-import { getSubscriberDetailsAsync } from '../../redux/subscriberSlice';
-import { getCurrencyValueAsync } from '../../redux/productSlice';
-import { countryDetails } from '../../data';
-import CategoryDropdown from '../UiElements/CategoryDropdown';
-
 import { CiLogin } from 'react-icons/ci';
 import { FaRegUser } from 'react-icons/fa6';
 import { BsCart2 } from 'react-icons/bs';
-import { BiUserCircle } from "react-icons/bi";
 
-
+import CategoryDropdown from '../UiElements/CategoryDropdown';
 
 const Navbar = () => {
   const navigate = useNavigate();
-
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categories.category);
   const loading = useSelector((state) => state.categories.loading);
-  const { user, userDetails } = useSelector((state) => state.subscribers);
+  const { user } = useSelector((state) => state.subscribers);
   const { cartItems } = useSelector((state) => state.cart);
-
   const [searchKey, setSearchKey] = useState('');
+
   useEffect(() => {
     if (user) {
-      dispatch(getSubscriberDetailsAsync());
+      // If user is logged in, fetch user details
+      // dispatch(getSubscriberDetailsAsync());
     }
   }, [user]);
 
   useEffect(() => {
-    dispatch(getCategoriesAsync());
-  }, []);
+    dispatch(getCategoriesAsync()); // Fetch categories
+  }, [dispatch]);
 
   if (loading) return <NavSkeletons />;
 
   return (
     <>
-      {/* <Container>
-        <section className='topnavInfo'>
-          <div className='topnavInfo-right'>
-            {userDetails && (
-              <div className='topnavInfo-right_user'>{`Welcome ${userDetails?.name}`}</div>
-            )}
-          </div>
-        </section>
-      </Container> */}
-      <section className='navMainWrapper'>
+      <section className="navMainWrapper">
         <Container>
-          <nav className='navWrapper'>
-            <figure className='navWrapper-brand'>
-              <Link to='/'>
-                <img src={BrandImage} alt='' />
+          <nav className="navWrapper">
+            <figure className="navWrapper-brand">
+              <Link to="/">
+                <img src={BrandImage} alt="Brand Logo" />
               </Link>
             </figure>
             <CategoryDropdown categories={categories} />
-            <section > <Link to='/about'> <span className='navWrapper-about'>  About Us </span></Link></section>
-            <section className='navWrapper-search'>
+            <section>
+              <Link to="/about">
+                <span className="navWrapper-about">ABOUT US</span>
+              </Link>
+            </section>
+            <section className="navWrapper-search">
               <input
-                type='search'
-                name='search'
+                type="search"
+                name="search"
                 value={searchKey}
                 onKeyUp={(e) =>
                   e.key === 'Enter' && navigate('/search', { state: searchKey })
                 }
-                id=''
                 onChange={(e) => setSearchKey(e.target.value)}
-                placeholder='search here ...'
+                placeholder="Search here ..."
               />
               <button
-                className='bton bton--sm bton--primary  mobSearchBton'
+                className="bton bton--sm bton--primary mobSearchBton"
                 onClick={(e) => {
                   e.preventDefault();
                   navigate('/search', { state: searchKey });
                 }}
               >
-                <span className='mobsearch'>search</span>
+                <span className="mobsearch">Search</span>
               </button>
             </section>
-            <section className='navWrapper-carts'>
-          
+            <section className="navWrapper-carts">
               {user ? (
-                <span className='navWrapper-carts-login'>
-                  <Link to='/account'>
-                    <span className=' me-1'>
-                      <FaRegUser />{' '}
+                <span className="navWrapper-carts-login">
+                  <Link to="/account">
+                    <span className="me-1">
+                      <FaRegUser />
                     </span>
                     <span>My Account</span>
                   </Link>
                 </span>
               ) : (
-                <span className='navWrapper-carts-login'>
-                  <Link to='/login'>
-                    <span className=' me-1'>
+                <span className="navWrapper-carts-login">
+                  <Link to="/login">
+                    <span className="me-1">
                       <CiLogin />
-                      {/* <FaRegUser /> */}
-                      {/* <BiUserCircle /> */}
                     </span>
                     <span>LOGIN</span>
                   </Link>
                 </span>
               )}
-                <span className='myCart'>
-                <Link to='/cart'>
+              <span className="myCart">
+                <Link to="/cart">
                   <BsCart2 />
                   &nbsp;
-                  <div className='cartCount'>{cartItems?.length}</div>
+                  <div className="cartCount">{cartItems?.length}</div>
                 </Link>
               </span>
             </section>
           </nav>
         </Container>
+
         <section>
-          <nav className='navbarBox'>
+          <nav className="navbarBox">
             <Container>
-              <ul className='menu p-0 m-0'>
+              <ul className="menu p-0 m-0">
                 {categories &&
                   categories.map((cat) => {
-                    const data = {
+                    const categoryData = {
                       cat_id: cat._id,
                       subData: null,
                       sub2Data: null,
                     };
+
                     return (
-                      <>
-                        <li className='menu--items'>
-                          <Link
-                            to={`/category/${cat.slug}`}
-                            state={data}
-                            className='navitems'
-                          >
-                            {cat.name}{' '}
-                          </Link>
-                          {cat.subCategory.length > 0 && (
-                            <img src={Chevrone} alt='' />
-                          )}
-                          {cat.subCategory.length > 0 && (
-                            <section className='sub--menu'>
-                              {cat.subCategory &&
-                                cat.subCategory.map((sub) => {
-                                  const data = {
-                                    cat_id: cat._id,
-                                    subData: { _id: sub._id, name: sub.name },
-                                    sub2Data: null,
-                                  };
-                                  return (
-                                    <section className='subcatWrapper'>
-                                      <div className='subcatWrapper--title'>
-                                        <Link
-                                          to={`/category/${cat.slug}`}
-                                          state={data}
-                                        >
-                                          {sub.name}
-                                        </Link>
-                                      </div>
-                                      <ul className='subcatWrapper--itemholder'>
-                                        {sub.subCategory2 &&
-                                          sub.subCategory2.map((sub2) => {
-                                            const data = {
-                                              cat_id: cat._id,
-                                              subData: {
-                                                _id: sub._id,
-                                                name: sub.name,
-                                              },
-                                              sub2Data: {
-                                                _id: sub2._id,
-                                                name: sub2.name,
-                                              },
-                                            };
-                                            return (
-                                              <li className='subcatWrapper--itemholder__items'>
-                                                <Link
-                                                  to={`/category/${cat.slug}`}
-                                                  state={data}
-                                                >
-                                                  {sub2.name}
-                                                </Link>
-                                              </li>
-                                            );
-                                          })}
-                                      </ul>
-                                    </section>
-                                  );
-                                })}
-                            </section>
-                          )}
-                        </li>
-                      </>
+                      <li className="menu--items" key={cat._id}>
+                        <Link
+                          to={`/category/${cat.slug}`} 
+                          state={categoryData} 
+                          className="navitems"
+                        >
+                          {cat.name}{' '}
+                        </Link>
+                        {cat.subCategory.length > 0 && (
+                          <img src={Chevrone} alt="Chevron" />
+                        )}
+                        {cat.subCategory.length > 0 && (
+                          <section className="sub--menu">
+                            {cat.subCategory.map((sub) => {
+                              const subData = {
+                                cat_id: cat._id,
+                                subData: { _id: sub._id, name: sub.name, slug: sub.slug },
+                                sub2Data: null,
+                              };
+
+                              return (
+                                <section className="subcatWrapper" key={sub._id}>
+                                  <div className="subcatWrapper--title">
+                                    <Link
+                                      to={`/category/${cat.slug}/${sub.slug}`} 
+                                      state={subData} 
+                                    >
+                                      {sub.name}
+                                    </Link>
+                                  </div>
+                                  {sub.subCategory2 && (
+                                    <ul className="subcatWrapper--itemholder">
+                                      {sub.subCategory2.map((sub2) => {
+                                        const sub2Data = {
+                                          cat_id: cat._id,
+                                          subData: { _id: sub._id, name: sub.name, slug: sub.slug },
+                                          sub2Data: { _id: sub2._id, name: sub2.name, slug: sub2.slug },
+                                        };
+                                        return (
+                                          <li
+                                            className="subcatWrapper--itemholder__items"
+                                            key={sub2._id}
+                                          >
+                                              {sub2.name}
+                                            
+                                          </li>
+                                        );
+                                      })}
+                                    </ul>
+                                  )}
+                                </section>
+                              );
+                            })}
+                          </section>
+                        )}
+                      </li>
                     );
                   })}
-                {/* <li className='menu--items'>
-                  <Link className='navitems' to={`/cleaning`}>
-                    Cleaning Facilities
-                  </Link>
-                </li> */}
               </ul>
             </Container>
           </nav>
         </section>
-         {/* <Category/> */}
       </section>
 
-      <section className='mobileWrapper'>
-        <section className='mobileNavbar'>
+      {/* Mobile Navbar */}
+      <section className="mobileWrapper">
+        <section className="mobileNavbar">
           <section>
             <NavLink
               className={(navData) =>
                 navData.isActive ? 'mobActiveTab' : 'notActive'
               }
-              to='/'
+              to="/"
             >
-              <section className='mobileNavbar-items'>
-                <i className='fa fa-home'></i>
-                <div className='mobileNavbar-items-name'>Home</div>
+              <section className="mobileNavbar-items">
+                <i className="fa fa-home"></i>
+                <div className="mobileNavbar-items-name">Home</div>
               </section>
             </NavLink>
           </section>
@@ -230,11 +201,11 @@ const Navbar = () => {
               className={(navData) =>
                 navData.isActive ? 'mobActiveTab' : 'notActive'
               }
-              to='/mobcat'
+              to="/mobcat"
             >
-              <section className='mobileNavbar-items'>
-                <i className='fa fa-sliders'></i>
-                <div className='mobileNavbar-items-name'>Categories</div>
+              <section className="mobileNavbar-items">
+                <i className="fa fa-sliders"></i>
+                <div className="mobileNavbar-items-name">Categories</div>
               </section>
             </NavLink>
           </section>
@@ -243,18 +214,15 @@ const Navbar = () => {
               className={(navData) =>
                 navData.isActive ? 'mobActiveTab' : 'notActive'
               }
-              to='/cart'
+              to="/cart"
             >
-              <section className='mobileNavbar-items'>
-                <i
-                  className='fa  fa-shopping-cart
-'
-                ></i>
-                <div className='mobileNavbar-items-name'>Cart</div>
+              <section className="mobileNavbar-items">
+                <i className="fa fa-shopping-cart"></i>
+                <div className="mobileNavbar-items-name">Cart</div>
                 {cartItems?.length > 0 && (
-                  <div className='cartMobCount'>{cartItems.length}</div>
+                  <div className="cartMobCount">{cartItems.length}</div>
                 )}
-              </section>{' '}
+              </section>
             </NavLink>
           </section>
           <section>
@@ -262,21 +230,16 @@ const Navbar = () => {
               className={(navData) =>
                 navData.isActive ? 'mobActiveTab' : 'notActive'
               }
-              to='/more'
+              to="/more"
             >
-              <section className='mobileNavbar-items'>
-                <i
-                  className='fa  fa-gear
-'
-                ></i>
-                <div className='mobileNavbar-items-name'>More</div>
-              </section>{' '}
+              <section className="mobileNavbar-items">
+                <i className="fa fa-gear"></i>
+                <div className="mobileNavbar-items-name">More</div>
+              </section>
             </NavLink>
           </section>
         </section>
-        
       </section>
-      
     </>
   );
 };
